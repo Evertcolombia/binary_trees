2e08273e8894b23d3ec66520be8024a462d75141
evertcolombia <evertescalante@gmail.com>
2020-03-09 22:21:42 +0000

    hotfix

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-binary_tree_t *create_node(binary_tree_t *new_node, int value);
-
 /**
  * binary_tree_node - create a node
  * @parent: root node for this child
@@ -13,31 +11,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
 
-	if (parent == NULL)
-	{
-		new_node = create_node(new_node, value);
-		new_node->parent = NULL;
-	}
-
-	new_node = create_node(new_node, value);
-	new_node->parent =  parent;
-
-	return (new_node);
-}
-
-/**
- * create_node - creates a node new
- * @new_node: new node to create
- * @value: value for the new node
- *
- * Return: new_node
- */
-binary_tree_t *create_node(binary_tree_t *new_node, int value)
-{
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-
+	new_node->parent = parent;
 	new_node->n = value;
+
 	return (new_node);
 }
