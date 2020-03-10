afe11757f84184610e74854941bb226adf4155e8
evertcolombia <evertescalante@gmail.com>
2020-03-10 19:00:25 +0000

    hotfix

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_is_leaf - know if a nod is leaf
+ * @node: node to ask
+ *
+ * Return: 1 on success
+ */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
