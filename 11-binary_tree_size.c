6eb5593be96e4a48e8a9eb1b8833cdbf2e27af12
evertcolombia <evertescalante@gmail.com>
2020-03-11 05:41:10 +0000

    hotfix

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,20 +1,28 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_size - measure the size of a root node in a b_tree
+ * @tree: node in bnary_tree
+ *
+ * Return: size of root node in binary_tree
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t s_left  = 0, s_right = 1;
+	size_t s_left  = 0, s_right = 0, res = 0;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL)
 		s_left = 0;
 	else
-		s_left = binary_tree_size(tree->left) + 1;
+		s_left = binary_tree_size(tree->left);
 
 	if (tree->right == NULL)
 		s_right = 0;
 	else
-		s_right = binary_tree_size(tree->right) + 1;
+		s_right = binary_tree_size(tree->right);
 
-	return (s_left + s_right);
+	res = s_left + s_right + 1;
+	return (res);
 }
