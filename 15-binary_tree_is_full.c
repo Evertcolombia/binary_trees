adfb51a80307994df72641eabcc1ea75681eeb8f
evertcolombia <evertescalante@gmail.com>
2020-03-12 18:30:19 +0000

    15- function to cjeck is a node es full or a tree is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,43 +1,18 @@
 #include "binary_trees.h"
-#include <stdio.h>
-int binary_tree(const binary_tree_t *tree);
+
+/**
+ * binary_tree_is_full - lok if a node is full in a b_Tree
+ * @tree: node to ask in a bnary_tree
+ *
+ * Return: 1 on success 0 on failure
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int h_left = 0, h_right = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		h_left = binary_tree(tree->left);
-	if(tree->right)
-		h_right = binary_tree(tree->left);
-	
-	printf("left: %d\n", h_left);
-	printf("right: %d\n", h_right);
-	if (h_left == h_right)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	else
-		return(0);
 
-}
-
-int binary_tree(const binary_tree_t *tree)
-{
-	int h_left = 0, h_right = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left)
-	{
-		if (tree->left && tree->right)
-			h_left = binary_tree(tree->left)  + 1;
-	}
-	if (tree->right)
-	{
-		if (tree->left && tree->right)
-			h_right = binary_tree(tree->right) + 1;
-	}
-	return(h_left + h_right);
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
